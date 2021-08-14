05518c87db8a62ae3d834fa2f3eb368230962030
Liang Jinyong <Langjinyong@tinteccnc.com>
2021-08-14 15:06:34 +0900

    First Commit

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //private ILoggerManager _logger;
+
+        //public WeatherForecastController(ILoggerManager logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly IRepositoryManager _repository;
+
+        public WeatherForecastController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //_logger.LogInfo("Here is info message from our values controller.");
+            //_logger.LogDebug("Here is debug message from our values controller.");
+            //_logger.LogWarn("Here is warn message from our values controller.");
+            //_logger.LogError("Here is an error message from our values controller.");
+
+            _repository.Compan
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
